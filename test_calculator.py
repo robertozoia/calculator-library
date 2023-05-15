f1e987f4febb4331040d6be41c63d26bc83e6bc7
Roberto Zoia <roberto@zoia.org>
2023-05-15 06:07:38 -0500

    Fixed typo in test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -9,5 +9,5 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-    def text_substraction(self):
+    def test_substraction(self):
         assert 2 == calculator.substract(4, 2)
